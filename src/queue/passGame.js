7b9e0343f886d8b0a3b8f27cce9b5e0fe343df18
lesson <1565510080@qq.com>
2022-06-14 17:07:47 +0800

    fear: 击鼓传花 注释

--- src/queue/passGame.js
+++ src/queue/passGame.js
@@ -13,8 +13,7 @@ export default function passGame(nameList, targetNumber = 0) {
 
   // 不确定循环的次数用while
   while (queue.size() > 1) {
-
-    /* 开始数数 */
+    /* 开始数数 把每一个出队的 在入队 遵循 先入先出 */
     for (let i = 0; i < targetNumber - 1; i++) {
       /* 把目标元素之前的先取出来 在追加到队列中去*/
       queue.enqueue(queue.dequeue());
